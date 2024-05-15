b76213a21b08e9b89616174db626f933af28dd7f
Shashank Godiyal <godiyalshashank@gmail.com>
2024-05-15 14:45:22 +0530

    Update Base.js

--- src/pages/Base.js
+++ src/pages/Base.js
@@ -8,7 +8,7 @@ const Base = () => {
 
         <React.Fragment>
 
-            <div className="d-flex w-75 mx-auto padding0 border rounded px-4 py-4 lightgray mt-5" >
+            <div className="d-flex w-75 mx-auto padding0 border rounded px-4 py-4 lightgray my-5" >
 
                 <div className="container w-50 mx-0" >
 
